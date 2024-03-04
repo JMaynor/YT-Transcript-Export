602f97e919590255032c117557126c372f1d1cd3
Jordan Maynor <mail@jtm.li>
2024-03-04 14:21:11 -0600

    Rewrote refresh videos func to check what was already in DB

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ Main program
 """
 
 import json
+import os
 import sqlite3
 
+import apprise
 import yt_dlp as yt
 
 # Import JSON config file
@@ -14,6 +16,9 @@ try:
 except:
     print("Error reading config file")
     exit(1)
+
+apobj = apprise.Apprise()
+[apobj.add(x) for x in config["apprise_endpoints"] if "apprise_endpoints" in config]
 
 
 class Database:
@@ -26,11 +31,16 @@ class Database:
         self.conn.commit()
 
     def insert(self, table_name, columns, values):
-        self.cursor.execute(f"INSERT INTO {table_name} ({columns}) VALUES ({values})")
+        placeholders = ", ".join("?" * len(values))
+        self.cursor.execute(
+            f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})", values
+        )
         self.conn.commit()
 
-    def select(self, table_name, columns, condition):
-        self.cursor.execute(f"SELECT {columns} FROM {table_name} WHERE {condition}")
+    def select(self, table_name, columns, condition, params):
+        self.cursor.execute(
+            f"SELECT {columns} FROM {table_name} WHERE {condition}", params
+        )
         return self.cursor.fetchall()
 
     def query(self, query):
@@ -59,38 +69,82 @@ def setup_database(db: Database):
     )
 
 
-def refresh_videos(db: Database):
+def refresh_channels(db: Database):
     """
-    Goes through channels in the config file and adds any new videos to the database
+    Goes through channels in the config file and adds any new channels to the database
     """
     for channel in config["channels"]:
-        # Get channel ID
         try:
-            channel_info = yt.YoutubeDL({}).extract_info(channel, download=False, process=False)
+            channel_info = yt.YoutubeDL({}).extract_info(
+                channel, download=False, process=False
+            )
         except Exception as e:
             print(f"Error getting channel info for {channel}: {e}")
             continue
 
         # Add channel to database if not present
-        if not db.select("channels", "id", f"id = '{channel_info['id']}'"):
+        if not db.select("channels", "id", "id = ?", (channel_info["id"],)):
             db.insert(
                 "channels",
-                "id, name",
-                f"'{channel_info['id']}', '{channel_info['channel']}'",
+                "id, name, url",
+                (
+                    channel_info["id"],
+                    channel_info["channel"],
+                    channel_info["channel_url"],
+                ),
             )
 
+
+def refresh_videos(db: Database):
+    """
+    Goes through channels in the config file and adds any new videos to the database
+    """
+    # For each channel in database
+    channels = db.select("channels", "id, url", "1", ())
+    for channel in channels:
+        channel_id = channel[0]
+        channel_url = channel[1]
+        # Get videos already in DB for channel
+        videos = db.select("videos", "id", "channelid = ?", (channel_id,))
+
+        # Create temp txt file with video IDs for yt-dlp to read
+        with open("temp.txt", "w") as f:
+            for video in videos:
+                f.write(f"{video[0]}\n")
+
+        ytdl_opts = config["ytdl_options"]
+        ytdl_opts["download_archive"] = "temp.txt"
+
+        # Download the info for the videos that are not in the download archive
+        with yt.YoutubeDL(ytdl_opts) as ydl:
+            try:
+                info_dict = ydl.extract_info(channel_url, download=False)
+            except Exception as e:
+                print(f"Error getting video info for {channel_id}: {e}")
+                continue
+
+        os.remove("temp.txt")
+
+        # Get the list of videos from the info dict
+        videos = info_dict["entries"]
+
         # Add videos to the database if not already present
-        # TODO: Change strategy here. Don't want to have to go through all videos every time
-        # Would be better to only pull video info that is not already in the database. Would
-        # be far faster on subsequent runs.
-        for video in channel_info["entries"]:
+        for video in videos:
             # Add video to database if not present
-            if not db.select("videos", "id", f"id = '{video["id"]}'"):
-                db.insert(
-                    "videos",
-                    "id, channelid, title, url",
-                    f"'{video["id"]}', '{channel_info['id']}', '{video['title']}', '{video['webpage_url']}'",
-                )
+            if not db.select("videos", "id", "id = ?", (video["id"],)):
+                try:
+                    db.insert(
+                        "videos",
+                        "id, channelid, title, url",
+                        (
+                            video["id"],
+                            channel_id,
+                            video["title"],
+                            video["webpage_url"],
+                        ),
+                    )
+                except Exception as e:
+                    print(f"Error adding video to database: {e}")
 
 
 def download_transcripts(db: Database):
@@ -111,8 +165,10 @@ if __name__ == "__main__":
 
     setup_database(db)
 
+    refresh_channels(db)
+
     refresh_videos(db)
 
-    download_transcripts(db)
+    # download_transcripts(db)
 
     db.close()
